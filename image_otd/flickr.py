0cfc43fbe4e78486e79a10dc046841549c5bd902
amerorchis <andrew.p.smith95@gmail.com>
2023-12-27 09:57:30 -0700

    Flickr retry if image not found

--- image_otd/flickr.py
+++ image_otd/flickr.py
@@ -5,6 +5,10 @@ from urllib.request import urlretrieve
 from pathlib import Path
 from os import environ
 
+if __name__ == "__main__":
+    from dotenv import load_dotenv
+    load_dotenv("email.env")
+
 flickr_key = environ['flickr_key']
 flickr_secret = environ['flickr_secret']
 glaciernps_uid = environ['glaciernps_uid']
@@ -18,7 +22,12 @@ def get_flickr():
     potd_num = random.randint(1, total)
     photos = flickr.photos.search(user_id=glaciernps_uid, per_page='1', page=potd_num)
 
+    while len(photos['photos']['photo']) == 0:
+        potd_num = random.randint(1, total)
+        photos = flickr.photos.search(user_id=glaciernps_uid, per_page='1', page=potd_num)
+
     selected = photos['photos']['photo'][0]
+    
     server, id, secret, title = selected['server'], selected['id'], selected['secret'], selected['title']
     pic_url = f'https://live.staticflickr.com/{server}/{id}_{secret}_c.jpg'
     save_loc = Path('email_images/today/raw_image_otd.jpg')
@@ -29,7 +38,5 @@ def get_flickr():
     return save_loc, title, link
 
 if __name__ == "__main__":
-    from dotenv import load_dotenv
-    load_dotenv("email.env")
     print(get_flickr())
     
