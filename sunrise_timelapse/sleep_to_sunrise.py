744132e0b7ef4df76c46ea4e1f81e0f44e933904
amerorchis <andrew.p.smith95@gmail.com>
2024-04-04 09:48:29 -0600

    Document sleep module

--- sunrise_timelapse/sleep_to_sunrise.py
+++ sunrise_timelapse/sleep_to_sunrise.py
@@ -1,25 +1,36 @@
-from datetime import datetime, date, timedelta, time
-from astral import LocationInfo, sun
-from zoneinfo import ZoneInfo
+"""
+Calculate when the sunrise timelapse will be finished and sleep until that time.
+"""
+
+from datetime import datetime, date, timedelta
 from time import sleep
+from zoneinfo import ZoneInfo
+from astral import LocationInfo, sun
 
 
 def sunrise_timelapse_complete_time():
+    """
+    Calculate the sunrise time and add 50 minutes.
+    """
     # Create West Glacier LI object
-    wg=LocationInfo(name='west glacier', region='MT', timezone='US/Mountain', latitude=48.4950, longitude=-113.9811)
+    wg=LocationInfo(name='west glacier', region='MT', timezone='US/Mountain', \
+                    latitude=48.4950, longitude=-113.9811)
 
     # Create West Glacier sun object
     s = sun.sun(wg.observer, date=date.today(), tzinfo=wg.timezone)
 
     now = datetime.now(tz=ZoneInfo('America/Denver'))
 
-    # Sunrise time minus now plus 50 minutes
-    timelapse_ready_in = s[f"sunrise"] - now + timedelta(minutes=50)
+    # Sunrise time minus now plus 52 minutes
+    timelapse_ready_in = s["sunrise"] - now + timedelta(minutes=52)
 
     return timelapse_ready_in.total_seconds()
 
 
 def sleep_time():
+    """
+    Sleep the program until sunrise timelapse is complete.
+    """
 
     timelapse_ready_in = sunrise_timelapse_complete_time()
 
