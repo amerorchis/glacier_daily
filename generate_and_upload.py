20f5c802649e53a0ee4f6e985d41cdbc3514f4e9
amerorchis <andrew.p.smith95@gmail.com>
2024-05-01 17:10:08 -0600

    Add web version

--- generate_and_upload.py
+++ generate_and_upload.py
@@ -25,6 +25,7 @@ from sunrise_timelapse.vid_frame import process_video
 from product_otd.product import get_product
 from notices.notices import get_notices
 from drip.html_friendly import html_safe
+from web_version import web_version
 
 
 def gen_data():
@@ -87,23 +88,22 @@ def gen_data():
 
     return drip_template_fields
 
-
-def send_to_server(data: dict, doctype: str) -> str:
-    """
-    Encode data in base64, add the date, then upload it to glacier.org using FTP.
-    :return A string of the URL it was updated too.
-    """
-
+def write_data_to_json(data: dict, doctype: str) -> str:
     data = {i: base64.b64encode(data[i].encode('utf-8')).decode('utf-8') for i in data.keys()}
 
     data['date'] = datetime.now().strftime('%Y-%m-%d')
     data['gnpc-events'] = get_gnpc_events()
-
-    with open(f"server/{doctype}.json", "w", encoding="utf-8") as f:
+    filepath = f"server/{doctype}"
+    with open(filepath, "w", encoding="utf-8") as f:
         f.write(json.dumps(data))
 
-    file_path = f'{doctype}.json'
-    directory = 'api'
+    return filepath
+
+def send_to_server(filepath: str, directory: str) -> str:
+    """
+    Encode data in base64, add the date, then upload it to glacier.org using FTP.
+    :return A string of the URL it was updated too.
+    """
 
     # Connect to the FTP server
     username = os.environ['FTP_USERNAME']
@@ -113,28 +113,30 @@ def send_to_server(data: dict, doctype: str) -> str:
     ftp.login(username, password)
     ftp.cwd(directory)
 
+    filename = filepath.split('/')[-1]
     try:
         # Open the local file in binary mode
-        with open(f'server/{doctype}.json', 'rb') as f:
+        with open(filepath, 'rb') as f:
             # Upload the file to the FTP server
-            ftp.storbinary('STOR ' + file_path, f)
+            ftp.storbinary('STOR ' + filename, f)
 
-    except Exception:
-        print('Failed upload JSON file.', file=sys.stderr)
+    except Exception as e:
+        print(f'Failed upload {filepath} file. {e}', file=sys.stderr)
 
     # Close the FTP connection
     ftp.quit()
 
-    return f'https://glacier.org/daily/{directory}/{file_path}'
+    return f'https://glacier.org/daily/{directory}/{filename}'
 
 
-def serve_api(doctype: str="email"):
+def serve_api():
     """
     Get the data, then upload it to server for API.
     """
     data = gen_data()
-    send_to_server(data, doctype)
-
+    web = web_version(data)
+    send_to_server(write_data_to_json(data, 'email.json'), 'api')
+    send_to_server(web, 'email')
 
 if __name__ == "__main__":
     serve_api()
